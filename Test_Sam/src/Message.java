44c05abe17d76b59cc56679193c2c03a44417f13
Md Junaid <Md Junaid@DESKTOP-TF80URS>
2019-08-28 16:37:31 -0400

    Added a class message2.java

--- Test_Sam/src/Message.java
+++ Test_Sam/src/Message.java
@@ -1,5 +1,5 @@
 
-public class Test {
+public class Message {
 	public static void main(String args[]){ 
 System.out.println("hello junaid");
 }}
---
+++ Test_Sam/src/Message2.java
@@ -0,0 +1,9 @@
+
+public class Message2 {
+	public static void main(String args[]){ 
+		System.out.println("hello junaid");
+		
+		System.out.println("Test Engineer");
+		}
+
+}
